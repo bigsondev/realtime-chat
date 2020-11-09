ef37ef44716bc535e2d8d66e1a5dd48537c01802
Adrian Bigaj <adrian.bigaj@clxcommunications.com>
2020-11-09 18:05:43 +0100

    Add my footer

---
+++ src/modules/realtime-chat/BigsonDevFooter.js
@@ -0,0 +1,50 @@
+import { Typography, Box, Link, makeStyles } from '@material-ui/core';
+import { Favorite } from '@material-ui/icons';
+
+const useStyles = makeStyles({
+  linkHolder: {
+    textDecoration: 'underline',
+  },
+});
+
+export const BigsonDevFooter = () => {
+  const { linkHolder } = useStyles();
+
+  return (
+    <Box mt={3}>
+      <Typography align="center" variant="body2" gutterBottom>
+        Made with <Favorite color="secondary" /> by{' '}
+        <Link
+          href="https://bigsondev.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={linkHolder}
+        >
+          BigsonDev
+        </Link>
+        . Source code available on{' '}
+        <Link
+          href="https://github.com/bigsondev/realtime-chat"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={linkHolder}
+        >
+          GitHub
+        </Link>
+        .
+      </Typography>
+      <Typography align="center" variant="body2">
+        {`Let's`}{' '}
+        <Link
+          href="https://bigsondev.com/mentorship/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={linkHolder}
+        >
+          skyrocket
+        </Link>{' '}
+        in Frontend together 🚀
+      </Typography>
+    </Box>
+  );
+};
--- src/modules/realtime-chat/Footer.js
+++ src/modules/realtime-chat/Footer.js
@@ -3,6 +3,7 @@ import { Send } from '@material-ui/icons';
 
 import utilityStyles from '../../styles/utility.module.css';
 
+import { BigsonDevFooter } from './BigsonDevFooter';
 import realtimeChatStyles from './realtime-chat.module.css';
 
 const { chatFooterContainer, inputField, sendButton } = realtimeChatStyles;
@@ -14,19 +15,22 @@ export const Footer = ({
   onEnterKeyPress,
   onMessageSend,
 }) => (
-  <footer className={chatFooterContainer}>
-    <input
-      type="text"
-      name="messageText"
-      placeholder="Type something..."
-      className={inputField}
-      value={messageText}
-      onChange={onMessageChange}
-      onKeyUp={onEnterKeyPress}
-    />
-    <Send
-      onClick={onMessageSend}
-      className={classNames(sendButton, icon, ml16)}
-    />
-  </footer>
+  <>
+    <footer className={chatFooterContainer}>
+      <input
+        type="text"
+        name="messageText"
+        placeholder="Type something..."
+        className={inputField}
+        value={messageText}
+        onChange={onMessageChange}
+        onKeyUp={onEnterKeyPress}
+      />
+      <Send
+        onClick={onMessageSend}
+        className={classNames(sendButton, icon, ml16)}
+      />
+    </footer>
+    <BigsonDevFooter />
+  </>
 );
